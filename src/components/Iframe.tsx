27bc86e2a5460ac5d7eab7f3da5f634f52080e22
danny <danny.bennett@gmail.com>
2024-06-18 21:24:34 -0500

    just added a type for a prop

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -1,7 +1,10 @@
-function Iframe({ link }: { link: string }) {
+import ILink from "../types/link.type";
+
+// you could also cast it as { link: string }
+function Iframe({ link }: ILink) {
   return (
     <div>
-      <iframe src={link} width={400} height={400}></iframe>
+      <iframe src={link ?? ''} width={400} height={400}></iframe>
     </div>
   );
 }
